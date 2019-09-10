328f0ddfadf9361dff1e68e6e47310e9ba49c0ba
umetaman <info@umetaman.com>
2019-09-11 00:05:34 +0900

    SampleSceneにターゲットカメラを追加。Prefab化。

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField]
+    private Camera mainCamera;
+    [SerializeField]
+    private Transform targetObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(targetObject == null)
+        {
+            Debug.LogError("targetObject is Null.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetObject != null)
+        {
+            mainCamera.transform.LookAt(targetObject);
+        }
+    }
+}
