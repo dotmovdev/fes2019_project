564deb9449e0163abb68f25ff959bb1f28d53fd4
umetaman <info@umetaman.com>
2019-09-23 23:58:17 +0900

    流れ星の基礎は完成

---
+++ Assets/Scripts/VFXShootingStar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class VFXShootingStar : VFXBind
+{
+    private Vector3 prePosition = new Vector3(0, 0, 0);
+    private Vector3 particleVelocity;
+
+    private void Start()
+    {
+        prePosition = this.transform.position;
+    }
+
+    private void Update()
+    {
+        updateVFXProperty();
+
+        particleVelocity = this.transform.position - prePosition;
+
+        prePosition = this.transform.position;
+    }
+
+    protected override void updateVFXProperty()
+    {
+        vfx.SetVector3("_Position", this.transform.localPosition);
+        vfx.SetVector3("_Velocity", particleVelocity.normalized);
+    }
+}
