5bb091a89483548d784aa8273bdbc524d59ba524
umetaman <info@umetaman.com>
2019-09-23 23:56:44 +0900

    VFXBindを定義。ScriptとVFXの変数などを紐づけるときはこれを使う。イベント制御も可能。

---
+++ Assets/Scripts/VFXBind.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+using UnityEngine.Events;
+
+public abstract class VFXBind : MonoBehaviour
+{
+    [SerializeField]
+    protected VisualEffect vfx;
+
+    [Header("EventName")]
+    [SerializeField]
+    private string playEventName = "Play";
+    [SerializeField]
+    private string stopEventName = "Stop";
+
+    [Header("EventBind")]
+    public UnityEvent OnPlay;
+    public UnityEvent OnStop;
+
+    protected abstract void updateVFXProperty();
+    private void Update()
+    {
+        updateVFXProperty();
+    }
+
+    public void Play()
+    {
+        vfx.SendEvent(playEventName);
+        OnPlay.Invoke();
+    }
+
+    public void Stop()
+    {
+        vfx.SendEvent(stopEventName);
+        OnStop.Invoke();
+    }
+}
