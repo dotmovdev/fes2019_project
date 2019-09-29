6085f86e1768c6739b817b16373b501b4b837617
sakura_abe <p1622006@myu.ac.jp>
2019-09-29 16:13:31 +0900

    SL関連のエフェクトとマテリアルを調整

---
+++ Assets/Scripts/GIControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GIControl : MonoBehaviour
+{
+    [SerializeField]
+    private Renderer renderer;
+    [SerializeField]
+    Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DynamicGI.SetEmissive(renderer, color);
+    }
+}
