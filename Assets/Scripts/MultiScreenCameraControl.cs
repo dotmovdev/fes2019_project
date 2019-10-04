aadda243cb432ab73c56a9b9c705c803fbc30ece
umetaman <info@umetaman.com>
2019-10-04 14:10:14 +0900

    マルチスクリーンを有効にするScriptを追加。

---
+++ Assets/Scripts/MultiScreenCameraControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiScreenCameraControl : MonoBehaviour
+{
+    [Header("MultiScreenCamera")]
+    private List<Camera> ScreenCameras = new List<Camera>();
+
+    public int DisplayCount
+    {
+        get
+        {
+            return Display.displays.Length;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogFormat("<color=red>Display Connected: </color>{0}", DisplayCount);
+
+        if(DisplayCount > 1)
+        {
+            for(int i = 1; i < DisplayCount; i++)
+            {
+                Display.displays[i].Activate();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
