38f0bbd6f18dfe006d9ac9fb72aa4d940e2df85f
umetaman <info@umetaman.com>
2019-10-10 19:39:16 +0900

    UnityのAPIが不安定で、キャッシュに左右されるので、ウィンドウの大きさはNativePluginから変更することに。NativePluginクラスを別ファイルに移動。

--- Assets/Scripts/MultiScreenControl.cs
+++ Assets/Scripts/MultiScreenControl.cs
@@ -4,32 +4,6 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
-public static class NativePlugin
-{
-    [StructLayout(LayoutKind.Sequential)]
-    public struct RECT
-    {
-        public int left;
-        public int top;
-        public int right;
-        public int buttom;
-    }
-
-    [DllImport("user32.dll", EntryPoint = "FindWindow")]
-    public static extern int FindWindow(String className, String windowName);
-
-    [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
-    public static extern int SetWindowPos(int hwnd, int hwndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
-
-    [DllImport("user32.dll", EntryPoint = "GetDesktopWindow")]
-    public static extern int GetDesktopWindow();
-
-    [DllImport("user32.dll", EntryPoint = "GetWindowRect")]
-    public static extern bool GetWindowRect(int hWnd, out RECT rect);
-
-    
-}
-
 public class MultiScreenControl : MonoBehaviour
 {
     [SerializeField]
@@ -43,40 +17,27 @@ public class MultiScreenControl : MonoBehaviour
         }
     }
 
-    [System.Serializable]
-    public struct ScreenSetting
-    {
-        public int Width;
-        public int Height;
-    }
-
-    [Header("Screen Settings")]
-    [SerializeField]
-    private List<ScreenSetting> screenSettings = new List<ScreenSetting>();
-
     public void ActivateDisplays()
     {
-        Display.displays[0].Activate();
-        Display.displays[1].Activate();
+        for(int i = 0; i < DisplayCount && i < 2; i++)
+        {
+            Display.displays[i].Activate();
+        }
     }
 
     public void FixSecondaryWindow()
     {
-        int firstWindowHandle;  //ウィンドウ
-        int secondWindowHandle;  //ウィンドウ
+        int windowHandle;  //ウィンドウ
 
-        const String FIRST_WINDOW_NAME = "fes2019";
-        const String SECOND_WINDOW_NAME = "Unity Secondary Display";
+        const String WINDOW_NAME = "fes2019";
 
         //常に最前面に表示
         const int HWND_TOPMOST = -1;
         //ウィンドウを表示
         const int SWP_SHOWWINDOW = 0x0040;
 
-        firstWindowHandle = NativePlugin.FindWindow(null, FIRST_WINDOW_NAME);
-        secondWindowHandle = NativePlugin.FindWindow(null, SECOND_WINDOW_NAME);
+        windowHandle = NativePlugin.FindWindow(null, WINDOW_NAME);
 
-        NativePlugin.SetWindowPos(firstWindowHandle, HWND_TOPMOST, 0, 0, 1920 * 2, 1080, SWP_SHOWWINDOW);
-        NativePlugin.SetWindowPos(secondWindowHandle, HWND_TOPMOST, 1920 * 2, 0, 1920/2, 1080, SWP_SHOWWINDOW);
+        NativePlugin.SetWindowPos(windowHandle, HWND_TOPMOST, 0, 0, 1920 * 2, 1080, SWP_SHOWWINDOW);
     }
 }
