230d53e43e49251214472c5ee1e5e1ec69773000
umetaman <info@umetaman.com>
2019-10-11 10:56:44 +0900

    Inspectorから色を変更できるようになった

---
+++ Assets/Scripts/SaveColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+［CreateAssetMenu(menuName = "MyScriptable/Create SaveColor")］;
+
+public class SaveColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
